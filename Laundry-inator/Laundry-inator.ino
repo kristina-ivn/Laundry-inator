957ecfd1034bb9746bbcee417d85f34fe97120d9
KristinaIvn <kris.tina.ivn.07@gmail.com>
2025-07-04 11:05:39 +0300

    Update Laundry-inator.ino

--- Laundry-inator/Laundry-inator.ino
+++ Laundry-inator/Laundry-inator.ino
@@ -1,24 +1,24 @@
 #include <Servo.h>
-Servo s1;
-int val=0;
-void setup()
-{
-  pinMode(6,INPUT);
-  s1.attach(7);
-}
-void loop()
-{
-val= digitalRead(7);
-Serial.println(val);
-delay(500);
 
-if (val == 0)
-{
-s1.write(90);
-delay(500);
+Servo s1;
+int val = 0;
+
+void setup() {
+  pinMode(6, INPUT);     
+  s1.attach(7);          
+  Serial.begin(9600);    
 }
-if (val == 1)
-{
-  s1.write(0);
-}
+
+void loop() {
+  val = digitalRead(6); 
+  Serial.println(val);
+  delay(1000);
+
+  if (val == 0) {
+    s1.write(0);        
+    delay(1000);
+  }
+  if (val == 1) {
+    s1.write(90);        
+  }
 }
